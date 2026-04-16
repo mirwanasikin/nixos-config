e1127fb9cc15a3e6f1c0dc68dec1d98829f9d954
M. Irwan Asikin <225542013+mirwanasikin@users.noreply.github.com>
2026-04-16 14:25:10 +0700

    fix: gtk theme and http scheme from google chrome to brave
    fix: mimeapps fix on x-scheme-handler

--- home/modules/gtk.nix
+++ home/modules/gtk.nix
@@ -1,23 +1,23 @@
 { pkgs, config, ... }:
 {
-    gtk = {
-        enable = true;
-        gtk4.theme = config.gtk.theme;
-        theme = {
-            name = "Catppuccin-Mocha-Blue-Dark";
-            package = pkgs.catppuccin-gtk;
-        };
-        iconTheme = {
-            name = "Tela-dracula";
-            package = pkgs.tela-circle-icon-theme;
-        };
-
-        cursorTheme = {
-            name = "Bibata-Modern-Ice";
-            package = pkgs.bibata-cursors;
-            size = 24;
-        };
+  gtk = {
+    enable = true;
+    gtk4.theme = config.gtk.theme;
+    theme = {
+      name = "catppuccin-mocha-blue-standard";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "blue" ];
+        variant = "mocha";
+      };
     };
-
+    iconTheme = {
+      name = "Tela-dracula";
+      package = pkgs.tela-circle-icon-theme;
+    };
+    cursorTheme = {
+      name = "Bibata-Modern-Ice";
+      package = pkgs.bibata-cursors;
+      size = 24;
+    };
+  };
 }
-
