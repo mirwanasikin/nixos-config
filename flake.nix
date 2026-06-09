efec17d01892403271e7913f11131b40aa973cb7
M. Irwan Asikin <225542013+mirwanasikin@users.noreply.github.com>
2026-06-09 17:19:18 +0700

    testing: Master Branch
    Warning this is not just usual experimental so if you're avoiding chaos
    don't use this branch

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Irwan NixOS Setup";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
