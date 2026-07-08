22d675c13007eaad1e44a9eb7bde0682c839c819
M. Irwan Asikin <225542013+mirwanasikin@users.noreply.github.com>
2026-07-08 12:28:26 +0700

    chore(pkgs): system update

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Irwan NixOS Setup";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
